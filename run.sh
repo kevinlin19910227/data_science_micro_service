035a3ea13331ec9a444bf71aec3f71db9a883e0c
Kevin Lin <kevin19910227@gmail.com>
2020-01-21 19:38:44 +0800

    test

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#pip install torch pytorch-pretrained-bert
+pip install torch pytorch-pretrained-bert
 python3 src/app.py --model_name bert_ssc --state_dict_path state_dict/bert_ssc_val_acc
 #docker run -v ${PWD}/logs:/logs ms:1.0.1 python3 src/app.py --model_name bert_ssc --state_dict_path state_dict/bert_ssc_val_acc
 #docker run -p 5000:5000 -v ${PWD}/logs:/logs ms:1.0.1 python3 src/app.py --model_name bert_ssc --state_dict_path state_dict/bert_ssc_val_acc
